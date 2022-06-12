b07af9b0a71f5f7cc02ab0ef88910c4a0de7944b
Toxa <antohanike@gmail.com>
2022-06-12 18:49:08 +0300

    Initial Commit

---
+++ testBet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testBet
+//
+//  Created by Тоха on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
